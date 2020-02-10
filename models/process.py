be956dfbe672425f705ee0cba8236b34305f7f7c
cavesdev <24899540+cavesdev@users.noreply.github.com>
2020-02-10 17:54:55 -0600

    - Added GUI to show results - Graph colors are now random - Graph bars now have black edges - Refactored all code to OOP - Updated .gitignore and requirements.txt

--- models/process.py
+++ models/process.py
@@ -5,17 +5,23 @@ class Process:
         self.duration = duration
         self.number = Process.number
         self.times = []
+        self.waiting_time = 0
         Process.number += 1
 
     def get_remaining_time(self):
         return self.duration
 
-    def reduce_time(self, time_reduced):
+    def reduce_duration(self, time_reduced):
         self.duration -= time_reduced
-        return self.duration
 
-    def add_time(self, time, duration):
-        self.times.append((time, duration))
+    def add_processing_time(self, begin, duration):
+        self.times.append((begin, duration))
 
-    def get_times(self):
+    def get_processing_times(self):
         return self.times
+
+    def add_waiting_time(self, time_added):
+        self.waiting_time += time_added
+
+    def get_waiting_time(self):
+        return self.waiting_time
--- round-robin.py
+++ round-robin.py
@@ -1,67 +1,109 @@
 import matplotlib.pyplot as plt
+import tkinter as tk
+import random
+from PIL import ImageTk, Image
 from models.process import Process
 
 
-def get_process_times(process_list):
-    sum = 0
-    for process in process_list:
-        sum += process.get_remaining_time()
-    return sum
+class RoundRobin:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title('Round Robin')
+        self.process_text = 'PROCESO\t RAFAGA\n'
+        self.process_number = 0
+        self.process_list = []
+        self.quantum = 0
+        self.passed_time = 0
+        self.total_time = 0
+        _, self.gnt = plt.subplots()
+
+    def ask_input(self):
+        self.process_number = int(input('Numero de procesos a crear: '))
+
+        # get process times
+        for i in range(self.process_number):
+            duration = int(input(f'Tiempo de rafaga del proceso {i + 1}: '))
+            new_process = Process(duration)
+            self.process_list.append(new_process)
+
+        self.quantum = int(input('Quantum: '))
+        self.total_time = self.get_process_times()
+
+    def get_process_times(self):
+        sum = 0
+        for process in self.process_list:
+            sum += process.get_remaining_time()
+            self.process_text += f'P{process.number}\t {process.duration}\n'
+        return sum
+
+    def run(self):
+        for process in self.process_list:
+            if process.get_remaining_time() > self.quantum:
+                process.reduce_duration(self.quantum)
+                process.add_processing_time(begin=self.passed_time, duration=self.quantum)
+                self.process_list.append(process)
+                self.passed_time += self.quantum
+                self.add_waiting_times(actual_process=process, time=self.quantum)
+            else:
+                process.add_processing_time(begin=self.passed_time, duration=process.get_remaining_time())
+                self.passed_time += process.get_remaining_time()
+                self.add_waiting_times(actual_process=process, time=process.get_remaining_time())
+
+    def add_waiting_times(self, actual_process, time):
+        for process in self.process_list:
+            if process is actual_process:
+                continue
+            process.add_waiting_time(time)
+
+    def graph(self):
+        self.gnt.set_ylim(0, 50)
+        self.gnt.set_xlim(0, self.total_time)
+        self.gnt.set_xlabel('seconds since start')
+        self.gnt.set_yticks([15, 25, 35])
+        self.gnt.set_yticklabels(['1', '2', '3'])
+
+        for process in set(self.process_list):
+            color = get_random_color()
+            time_list = process.get_processing_times()
+            self.gnt.broken_barh(time_list, (20, 9), color=color, label=f'P{process.number}', edgecolor='black')
+
+        self.gnt.legend()
+        plt.savefig('gantt_rr.png')
+
+    def show(self):
+        # add graph at East
+        img = ImageTk.PhotoImage(Image.open('gantt_rr.png'))
+        img_panel = tk.Label(self.window, image=img)
+        img_panel.grid(column=1, row=0)
+
+        # add process times at West
+        label = tk.Label(self.window, text=self.process_text)
+        label.grid(column=0, row=0)
+
+        # add waiting times at SE
+        label = tk.Label(self.window, text=self.get_waiting_times_text())
+        label.grid(column=1, row=1)
+
+        self.window.mainloop()
+
+    def get_waiting_times_text(self):
+        waiting_text = 'PROCESO\t ESPERA\n'
+        for process in set(self.process_list):
+            waiting_text += f'P{process.number}\t {process.get_waiting_time()}\n'
+        return waiting_text
 
 
-print('Round Robin')
-process_number = int(input('Numero de procesos a crear: '))
-process_list = []
-
-# get process times
-for i in range(process_number):
-    time = int(input(f'Tiempo de rafaga del proceso {i + 1}: '))
-    new_process = Process(time)
-    process_list.append(new_process)
-
-quantum = int(input('Quantum: '))
-
-# initialize graph
-# Declaring a figure "gnt"
-fig, gnt = plt.subplots()
-
-# Setting Y-axis limits
-gnt.set_ylim(0, 50)
-
-# Setting X-axis limits
-gnt.set_xlim(0, get_process_times(process_list))
-
-# Setting labels for x-axis and y-axis
-gnt.set_xlabel('seconds since start')
-gnt.set_ylabel('Processor')
-
-# Setting ticks on y-axis
-gnt.set_yticks([15, 25, 35])
-# Labelling tickes of y-axis
-gnt.set_yticklabels(['1', '2', '3'])
-
-i = 0
-
-for process in process_list:
-    if process.get_remaining_time() > quantum:
-        process.reduce_time(quantum)
-        print(f'P{process.number} - {quantum}')
-        process.add_time(i, quantum)
-        process_list.append(process)
-        i += quantum
-    else:
-        print(f'P{process.number} - {process.get_remaining_time()}')
-        process.add_time(i, process.get_remaining_time())
-        i += process.get_remaining_time()
-
-colors = ['red', 'green', 'blue', 'orange', 'black']
-
-for process in set(process_list):
-    color = colors[process.number - 1]
-    lists = process.get_times()
-    gnt.broken_barh(lists, (20, 9), facecolors=f'{color}', label=f'P{process.number}')
+def get_random_color():
+    red = random.random()
+    green = random.random()
+    blue = random.random()
+    alpha = 1.0
+    return (red, green, blue, alpha)
 
 
-gnt.legend()
-plt.savefig('gantt_rr.png')
-plt.show()
+if __name__ == "__main__":
+    rr = RoundRobin()
+    rr.ask_input()
+    rr.run()
+    rr.graph()
+    rr.show()
